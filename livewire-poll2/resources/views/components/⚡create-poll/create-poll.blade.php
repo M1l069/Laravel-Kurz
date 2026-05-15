2b46fc13ca315eb4c4f1ad46f0e3fad93b55b5b3
Miloslav Šmíd <97102231+M1l069@users.noreply.github.com>
2026-05-16 01:18:43 +0200

    pridanie dvoch metod do create-poll.php na pridanie a odstránenie možnosti, pridaný array v danom súbore

--- livewire-poll2/resources/views/components/⚡create-poll/create-poll.blade.php
+++ livewire-poll2/resources/views/components/⚡create-poll/create-poll.blade.php
@@ -4,5 +4,20 @@
         <label for="">Poll Title</label>
         <input type="text" wire:model.live="title">
         Current title: {{ $title }}
+        <div class="mt-4 mb-4">
+            <button class="btn" wire:click.prevent="addOption()"> Pridať možnosť</button>
+        </div>
+
+        <div>
+        @foreach($options as $index => $option)
+            <div class="mb-4">
+                <label for="">Option {{ $index + 1 }}</label>
+                <div class="flex gap-2">
+                    <input type="text" wire:model="options.{{ $index }}">
+                    <button class="btn" wire:click.prevent="removeOption({{ $index }})">Odstrániť</button>
+                </div>
+            </div>
+        @endforeach
+        </div>
     </form>
 </div>
--- livewire-poll2/resources/views/components/⚡create-poll/create-poll.php
+++ livewire-poll2/resources/views/components/⚡create-poll/create-poll.php
@@ -1,8 +1,22 @@
 <?php
 
 use Livewire\Component;
+use Ds\Set;
 
 new class extends Component
 {
     public $title;
+    public $options = ['first'];
+
+    // bude volana len raz napr. na citanie z db
+//    public function mount($title) {}
+
+    public function addOption() {
+        $this->options[] = ''; //pridanie nového členu na koniec arrayu
+    }
+
+    public function removeOption($index) {
+        unset($this->options[$index]);
+        $this->options = array_values($this->options);
+    }
 };
