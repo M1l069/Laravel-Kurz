0513d351417b099cbaaf83d78a4dcfcc258d895d
Miloslav Šmíd <97102231+M1l069@users.noreply.github.com>
2026-05-17 17:58:00 +0200

    Vytvoreny projekt pre Job_Board, model pre Job, migrácia pre app_jobs, controller pre Job a vytvorená factory pre Job a následne pridaná do DBSeeder

---
+++ job-board/app/Models/Job.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    /** @use HasFactory<\Database\Factories\JobFactory> */
+    use HasFactory;
+
+    protected $table = 'app_jobs';
+    public static array $experience = ['entry', 'intermediate', 'senior'];
+    public static array $category = ['IT', 'Financie', 'Predaje', 'Marketing'];
+
+
+}
